55963eadc689427e74216f12264d8b5f1eb805bf
gastonedel <102158957+gastonedel@users.noreply.github.com>
2022-04-21 09:01:21 -0300

    Initial commit

---
+++ Assets/Scripts/While1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class While1 : MonoBehaviour
+{
+    public int counter;
+    public int counterMax;
+
+    public Text Txt_display;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        while (counter<counterMax)
+        {
+            Debug.Log("repeticion");
+            counter++;
+        }
+        Txt_display.text = "termino el while";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
